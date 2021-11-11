1b58d7de3a06fa27866a45e8006b65bc7bec3011
Miika Savela <miika.a.savela@gmail.com>
2021-11-11 20:37:35 +0200

    Add unit spawning from basic unit spawner

---
+++ Assets/Scripts/Buildings/UnitSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Mirror;
+
+public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
+{
+    [SerializeField] GameObject unitPrefab;
+    Vector3 unitSpawnPointPosition;
+    #region Server
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        unitSpawnPointPosition = transform.Find("SpawnPoint_Unit").position;
+    }
+
+    [Command]
+    void CmdSpawnUnit()
+    {
+
+        GameObject spawnedUnit = Instantiate(unitPrefab, unitSpawnPointPosition, Quaternion.identity);
+        NetworkServer.Spawn(spawnedUnit, connectionToClient);
+    }
+
+
+    #endregion
+
+    #region Client
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!hasAuthority) { return; }
+
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            CmdSpawnUnit();
+        }
+    }
+    #endregion
+}
--- Assets/Scripts/Networking/RtsNetworkManager.cs
+++ Assets/Scripts/Networking/RtsNetworkManager.cs
@@ -5,5 +5,17 @@ using Mirror;
 
 public class RtsNetworkManager : NetworkManager
 {
-    
+    [SerializeField] GameObject unitSpawnerPrefab;
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        GameObject unitSpawnerInstance = Instantiate(
+            unitSpawnerPrefab,
+            conn.identity.transform.position, 
+            conn.identity.transform.rotation);
+
+        NetworkServer.Spawn(unitSpawnerInstance, conn);
+    }
 }
