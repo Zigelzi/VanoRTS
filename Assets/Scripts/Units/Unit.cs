0f2eb4dbfcaea6a9db5d49f9d7da87058ffb7a3f
Miika Savela <miika.a.savela@gmail.com>
2021-11-12 19:17:17 +0200

    Add selecting single unit

---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Mirror;
+public class Unit : NetworkBehaviour
+{
+    [SerializeField] UnityEvent onSelected;
+    [SerializeField] UnityEvent onDeselected;
+
+
+    #region Client
+    [Client]
+    public void Select()
+    {
+        if(!hasAuthority) { return;  }
+        onSelected?.Invoke();
+    }
+
+    [Client]
+    public void Deselect()
+    {
+        if (!hasAuthority) { return; }
+        onDeselected?.Invoke();
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Units/UnitSelectionHandler.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UnitSelectionHandler : MonoBehaviour
+{
+    [SerializeField] LayerMask layerMask = new LayerMask();
+    Camera mainCamera;
+    List<Unit> selectedUnits = new List<Unit>();
+
+    void Start()
+    {
+        mainCamera = Camera.main;    
+    }
+
+    void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            ClearSelection();
+
+        }
+        else if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            SelectAllUnitsInArea();
+        }
+    }
+
+    void ClearSelection()
+    {
+        DeselectAllUnits();
+
+        selectedUnits.Clear();
+    }
+
+    void DeselectAllUnits()
+    {
+        foreach (Unit unit in selectedUnits)
+        {
+            unit.Deselect();
+        }
+    }
+
+    void SelectAllUnitsInArea()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask))
+        {
+            AddUnitToSelection(hit);
+        }
+    }
+
+    void AddUnitToSelection(RaycastHit hit)
+    {
+        if (hit.collider.TryGetComponent<Unit>(out Unit hitUnit)) {
+            if (!hitUnit.hasAuthority) { return; }
+            
+            selectedUnits.Add(hitUnit);
+            SelectAllUnits();
+        }
+    }
+
+    void SelectAllUnits()
+    {
+        foreach (Unit unit in selectedUnits)
+        {
+            unit.Select();
+        }
+    }
+
+}
