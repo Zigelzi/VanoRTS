7250a5625eec3755ca001edc83cb178bdf82cd69
Miika Savela <miika.a.savela@gmail.com>
2021-11-21 11:23:02 +0200

    Add healthbar to units

---
+++ Assets/Scripts/UI/UI_Billboard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Billboard : MonoBehaviour
+{
+    Transform camera;
+    void Start()
+    {
+        camera = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + camera.forward);
+    }
+}
