4df8741bb0a97857b5323649d6077c8ee57278d0
Miika Savela <miika.a.savela@gmail.com>
2021-12-04 09:58:37 +0200

    Add bank for managing player gold, resources UI and initialising the gold value in the game and UI

---
+++ Assets/Scripts/UI/UI_Gold.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Mirror;
+
+public class UI_Gold : MonoBehaviour
+{
+    TMP_Text goldText;
+    RtsNetworkBank bank;
+    RtsNetworkPlayer player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        goldText = GetComponentInChildren<TMP_Text>();     
+    }
+    private void Update()
+    {
+        if (player == null)
+        {
+            player = NetworkClient.connection.identity.GetComponent<RtsNetworkPlayer>();
+            
+            bank = player.GetComponent<RtsNetworkBank>();
+            bank.ClientOnGoldUpdate += SetCurrentGold;
+
+            SetCurrentGold(bank.StartingGold);
+        }
+    }
+
+    void OnDestroy()
+    {
+        bank.ClientOnGoldUpdate -= SetCurrentGold;
+    }
+
+    void SetCurrentGold(int newGoldValue)
+    {
+        Debug.Log("Setcurrentgold ran");
+        goldText.text = newGoldValue.ToString();
+    }
+}
