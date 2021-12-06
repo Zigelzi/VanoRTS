ccbefa83ef162bc00cf9d592a0f7bcb7a6af1743
Miika Savela <miika.a.savela@gmail.com>
2021-12-06 18:44:39 +0200

    Fix building nav obstacle sizes and add units move to random spot after spawning

--- Assets/Scripts/Buildings/UnitSpawner.cs
+++ Assets/Scripts/Buildings/UnitSpawner.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.AI;
 using Mirror;
+using System;
 
 [RequireComponent(typeof(Health))]
 public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
@@ -10,10 +12,18 @@ public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
     [SerializeField] GameObject unitPrefab;
     [SerializeField] Transform unitSpawnPointPosition;
     [SerializeField] [Range(0, 10)] int maxQueueSize = 5;
+    [SerializeField] float spawnMoveRange = 7f;
     [SerializeField] Queue<Unit> unitQueue = new Queue<Unit>();
+
+    [SerializeField]
+    [SyncVar(hook = nameof(ServerHandleQueueSizeUpdated))]
+    int currentQueueSize = 0;
 
     RtsNetworkPlayer player;
     PlayerBank bank;
+
+    public event Action<int> ServerOnUnitQueueSizeUpdated;
+    public event Action<Unit> ServerOnUnitQueued;
 
     #region Server
     public override void OnStartServer()
@@ -28,9 +38,14 @@ public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
     void QueueUnit()
     {
         Unit queuedUnit = unitPrefab.GetComponent<Unit>();
-        if (unitQueue.Count <= maxQueueSize && bank.HasGold(queuedUnit.BuildingCost))
+        if (unitQueue.Count < maxQueueSize && bank.HasGold(queuedUnit.BuildingCost))
         {
+            bank.ConsumeGold(queuedUnit.BuildingCost);
             unitQueue.Enqueue(queuedUnit);
+
+            currentQueueSize = unitQueue.Count;
+            ServerOnUnitQueued?.Invoke(queuedUnit);
+
             StartCoroutine(SpawnUnit(queuedUnit));
         }
     }
@@ -47,7 +62,24 @@ public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
     {
         GameObject spawnedUnitInstance = Instantiate(unitPrefab, unitSpawnPointPosition.position, Quaternion.identity);
         NetworkServer.Spawn(spawnedUnitInstance, connectionToClient);
-        unitQueue.Dequeue();  
+        unitQueue.Dequeue();
+        currentQueueSize = unitQueue.Count;
+
+        MoveSpawnedUnit(spawnedUnitInstance);
+    }
+
+    [Server]
+    void MoveSpawnedUnit(GameObject spawnedUnit)
+    {
+        NavMeshAgent navAgent = spawnedUnit.GetComponent<NavMeshAgent>();
+        Vector3 spawnOffset = UnityEngine.Random.insideUnitSphere * spawnMoveRange;
+        Vector3 spawnDestination = spawnedUnit.transform.position + spawnOffset;
+        navAgent.SetDestination(spawnDestination);
+    }
+
+    void ServerHandleQueueSizeUpdated(int oldQueueSize, int newQueueSize)
+    {
+        ServerOnUnitQueueSizeUpdated?.Invoke(newQueueSize);
     }
 
     #endregion
--- Assets/Scripts/UI/UI_BuildingQueue.cs
+++ Assets/Scripts/UI/UI_BuildingQueue.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UI_BuildingQueue : MonoBehaviour
 {
+    UnitSpawner unitSpawner;
+    TMP_Text unitQueueText;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        unitSpawner = GetComponentInParent<UnitSpawner>();
+        unitQueueText = GetComponentInChildren<TMP_Text>();
+
+        unitQueueText.text = "0";
+
+        unitSpawner.ServerOnUnitQueueSizeUpdated += SetCurrentQueueText;
+        unitSpawner.ServerOnUnitQueued += StartQueueTimer;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnDestroy()
     {
-        
+        unitSpawner.ServerOnUnitQueueSizeUpdated -= SetCurrentQueueText;
+        unitSpawner.ServerOnUnitQueued -= StartQueueTimer;
     }
+
+    void SetCurrentQueueText(int newQueueSize)
+    {
+        unitQueueText.text = newQueueSize.ToString();
+    }
+
+    void StartQueueTimer(Unit unit)
+    {
+
+    } 
 }
