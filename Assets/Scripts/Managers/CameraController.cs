8c8e5b3b0023a1e124a301223e952b2bbebf31e0
Miika Savela <miika.a.savela@gmail.com>
2021-12-11 12:04:26 +0200

    Add camera pan limits and fix the reverse camera facing issue

--- Assets/Scripts/Managers/CameraController.cs
+++ Assets/Scripts/Managers/CameraController.cs
@@ -8,6 +8,8 @@ public class CameraController : NetworkBehaviour
 {
     [SerializeField] [Range(0, 50f)] float panSpeed = 30f;
     [SerializeField] [Range(0, 50f)] float zoomSpeed = 20f;
+    [SerializeField] Vector2 screenXLimits = Vector2.zero;
+    [SerializeField] Vector2 screenYLimits = Vector2.zero;
 
     float panBorderThickness = 40f; // In px
     Transform cameraTransform;
@@ -88,22 +90,22 @@ public class CameraController : NetworkBehaviour
         Vector3 cameraPosition = cameraTransform.position;
         if (cursorPosition.y >= Screen.height - panBorderThickness)
         {
-            cameraPosition.z -= panSpeed * Time.deltaTime;
+            cameraPosition.z += panSpeed * Time.deltaTime;
         }
 
         if (cursorPosition.y <= panBorderThickness)
         {
-            cameraPosition.z += panSpeed * Time.deltaTime;
+            cameraPosition.z -= panSpeed * Time.deltaTime;
         }
 
         if (cursorPosition.x >= Screen.width - panBorderThickness)
         {
-            cameraPosition.x -= panSpeed * Time.deltaTime;
+            cameraPosition.x += panSpeed * Time.deltaTime;
         }
 
         if (cursorPosition.x <= panBorderThickness)
         {
-            cameraPosition.x += panSpeed * Time.deltaTime;
+            cameraPosition.x -= panSpeed * Time.deltaTime;
         }
 
         cameraTransform.position = cameraPosition;
@@ -111,20 +113,23 @@ public class CameraController : NetworkBehaviour
 
     void HandleKeyboardInput()
     {
-        cameraTransform.position -= new Vector3(previousInput.x, 0f, previousInput.y) * panSpeed * Time.deltaTime;
+        Vector3 cameraPosition = cameraTransform.position;
+        cameraPosition += new Vector3(previousInput.x, 0f, previousInput.y) * panSpeed * Time.deltaTime;
+
+        Debug.Log(cameraPosition);
+
+        cameraPosition = ClampCameraMovement(cameraPosition);
+
+        cameraTransform.position = cameraPosition;
     }
 
     Vector3 ClampCameraMovement(Vector3 cameraPosition)
     {
         // Play area isn't centered to 0,0 so take the offset into account when clamping the camera position
-        float leftBoundary = 0f; // Limit on X axis
-        float rightBoundary = 200f; // Limit on X axis
-        float topBoundary = 200f; // Limit on Z axis
-        float bottomBoundary = -50f; // Limit on Z axis
         Vector3 clampedCameraPosition = cameraPosition;
 
-        clampedCameraPosition.x = Mathf.Clamp(cameraPosition.x, leftBoundary, rightBoundary);
-        clampedCameraPosition.z = Mathf.Clamp(cameraPosition.z, bottomBoundary, topBoundary);
+        clampedCameraPosition.x = Mathf.Clamp(cameraPosition.x, screenXLimits.x, screenXLimits.y);
+        clampedCameraPosition.z = Mathf.Clamp(cameraPosition.z, screenYLimits.x, screenYLimits.y);
 
         return clampedCameraPosition;
     }
